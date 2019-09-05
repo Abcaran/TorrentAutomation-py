424ff367ce7700826b6d6ea8b2be6c1e975fe2cf
Samir <samirsaadi@gmail.com>
2019-09-05 19:32:09 -0300

    Building the scrapper

--- grabbtorrent.py
+++ grabbtorrent.py
@@ -8,53 +8,67 @@ from PyInquirer import style_from_dict, Token, prompt, Separator
 from examples import custom_style_1
 
 
-# def search_torrent(name, quality):
-#     url = f"https://1337x.to/search/{search_name}+{search_quality}/1/"
+def search_torrent(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    name = soup.find(
+        'table', {'class': 'table-list table table-responsive table-striped'})
+    # link = soup.find
+
+    print(page.content)
+
+
+name = 'number+23'
+quality = '1080'
+formatted_url = f'https://1337x.to/search/{name}+{quality}/1/'
+search_torrent(formatted_url)
+
 
 # def qbittorren_torrent(magnetic_link):
 
 
-def main():
-    questions = [
-        {
-            'type': 'list',
-            'message': 'Would you like to download a:',
-            'name': 'content_type',
-            'choices': [
-                {'name': 'Movie'},
-                {'name': 'Series'}
-            ],
-        },
-        {
-            'type': 'input',
-            'message': 'Type the name:',
-            'name': 'content_name',
-        },
-        {
-            'type': 'list',
-            'message': 'Choose the quality you desire:',
-            'name': 'content_quality',
-            'choices': [
-                {'name': '2160 - (4k)'},
-                {'name': '1080 - (FULL HD)'},
-                {'name': '720 - (HD)'},
-                {'name': '480 - (SD)'},
-            ]
-        },
-    ]
+# def main():
+#     questions = [
+#         {
+#             'type': 'list',
+#             'message': 'Would you like to download a:',
+#             'name': 'content_type',
+#             'choices': [
+#                 {'name': 'Movie'},
+#                 {'name': 'Series'}
+#             ],
+#         },
+#         {
+#             'type': 'input',
+#             'message': 'Type the name:',
+#             'name': 'content_name',
+#         },
+#         {
+#             'type': 'list',
+#             'message': 'Choose the quality you desire:',
+#             'name': 'content_quality',
+#             'choices': [
+#                 {'name': '2160 - (4k)'},
+#                 {'name': '1080 - (FULL HD)'},
+#                 {'name': '720 - (HD)'},
+#                 {'name': '480 - (SD)'},
+#             ]
+#         },
+#     ]
 
-    answers = prompt(questions, style=custom_style_1)
-    pprint(answers)
+#     answers = prompt(questions, style=custom_style_1)
+#     pprint(answers)
 
-    formatted_name = answers['content_name']
-    formatted_name = re.sub(r'[\W]', ' ', formatted_name).split()
-    formatted_name = '+'.join(formatted_name)
+#     formatted_name = answers['content_name']
+#     formatted_name = re.sub(r'[\W]', ' ', formatted_name).split()
+#     formatted_name = '+'.join(formatted_name)
 
-    quality = answers['content_quality'][:4].rstrip()
+#     quality = answers['content_quality'][:4].rstrip()
 
-    formatted_url = f"https://1337x.to/search/{formatted_name}+{quality}/1/"
+#     search_torrent(formatted_name, quality)
 
-    print(formatted_url)
+#     print(formatted_url)
 
 
-main()
+# main()
